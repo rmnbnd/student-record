5cd1c6a1eae14a84f7fb79a91ed8f762c4641f84
Roman Bondarchuk <rmnbnd@gmail.com>
2016-11-16 23:51:26 +0200

    Add session service to save google auth code

---
+++ src/main/java/student/record/service/SessionService.java
@@ -0,0 +1,22 @@
+package student.record.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+
+@Component
+public class SessionService {
+
+    private static final String SESSION_GOOGLE_AUTHORIZE_CODE = "googleAuthorizeCode";
+
+    @Autowired
+    private HttpSession httpSession;
+
+
+    public void addGoogleAuthorizeCode(String code) {
+        httpSession.removeAttribute(SESSION_GOOGLE_AUTHORIZE_CODE);
+        httpSession.setAttribute(SESSION_GOOGLE_AUTHORIZE_CODE, code);
+    }
+
+}
